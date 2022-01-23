e32399fb94be47a35b766808c93cfbee459f934f
sushant21103125 <98103587+sushant21103125@users.noreply.github.com>
2022-01-23 23:50:14 +0530

    Add files via upload

---
+++ Asg2_output_21103125.py
@@ -0,0 +1,69 @@
+OUTPUT
+
+Question-1
+
+part a
+Length of input string = 34
+part b
+egaugnal evtisnes esac a si nohtyP
+
+part c
+a case sensitve 
+
+part d
+new_string
+
+part e
+10
+
+part f
+Pythonisacasesensitvelanguage
+> 
+
+-----------------------------------------------------------------------------------------------------------------------------------------------
+
+Question-2
+
+Hey, Sushant Kumar here! 
+My SID is 21103125
+I am from CSE and my CGPA is 9.9
+PS C:\Users\sk\Desktop>
+
+-----------------------------------------------------------------------------------------------------------------------------------------------
+
+Question-3
+
+a&b :  8
+ a|b :  58
+ a^b :  50
+a<<2 :  224 	b<<2 : 40
+a>>2 :  14 	b>>2 : 0
+> 
+
+-----------------------------------------------------------------------------------------------------------------------------------------------
+
+Question-4
+
+Enter 1st no. : 8
+Enter 2nd no. : 33
+Enter 3rd no. : 23
+Highest no. = 33
+PS C:\Users\sk\Desktop>
+
+-----------------------------------------------------------------------------------------------------------------------------------------------
+
+Question-5
+
+Enter input string: hello name
+Yes
+PS C:\Users\sk\Desktop> 
+
+-----------------------------------------------------------------------------------------------------------------------------------------------
+
+Question-6
+
+Enter 1st length : 25
+Enter 2nd length : 39
+Enter 3rd length : 13
+NO
+PS C:\Users\sk\Desktop>       
